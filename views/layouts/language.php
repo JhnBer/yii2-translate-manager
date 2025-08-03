7767e204171de910296f7ea47bc58cd8b5fca64e
John Ber <32464352+JhnBer@users.noreply.github.com>
2025-08-03 04:56:39 +0200

    language.php bs5 fixes

--- views/layouts/language.php
+++ views/layouts/language.php
@@ -19,66 +19,73 @@ TranslateManagerAsset::register($this);
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
-    <head>
-        <meta charset="<?= Yii::$app->charset ?>"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <?= Html::csrfMetaTags() ?>
-        <title><?= Html::encode($this->title) ?></title>
-        <?php $this->head() ?>
-    </head>
-    <body>
-        <?php $this->beginBody() ?>
-        <div class="wrap">
-            <?php
-            NavBar::begin([
-                'brandLabel' => 'Lajax TranslateManager',
-                'brandUrl' => Yii::$app->homeUrl,
-                'options' => [
-                    'class' => 'navbar-inverse navbar-fixed-top',
-                ],
-            ]);
-            $menuItems = [
-                ['label' => Yii::t('language', 'Home'), 'url' => ['/']],
-                ['label' => Yii::t('language', 'Language'), 'items' => [
-                    ['label' => Yii::t('language', 'List of languages'), 'url' => ['/translatemanager/language/list']],
-                    ['label' => Yii::t('language', 'Create'), 'url' => ['/translatemanager/language/create']],
-                ]],
-                ['label' => Yii::t('language', 'Scan'), 'url' => ['/translatemanager/language/scan']],
-                ['label' => Yii::t('language', 'Optimize'), 'url' => ['/translatemanager/language/optimizer']],
-                ['label' => Yii::t('language', 'Im-/Export'), 'items' => [
-                    ['label' => Yii::t('language', 'Import'), 'url' => ['/translatemanager/language/import']],
-                    ['label' => Yii::t('language', 'Export'), 'url' => ['/translatemanager/language/export']],
-                ]],
-            ];
-            echo Nav::widget([
-                'options' => ['class' => 'navbar-nav navbar-right'],
-                'items' => $menuItems,
-            ]);
-            NavBar::end();
-            ?>
+<head>
+    <meta charset="<?= Yii::$app->charset ?>"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+<div class="wrap">
+    <?php
+    NavBar::begin([
+        'brandLabel' => 'Lajax TranslateManager',
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar-inverse navbar-dark bg-dark navbar-fixed-top',
+        ],
+    ]);
+    $menuItems = [
+        ['label' => Yii::t('language', 'Home'), 'url' => ['/']],
+        ['label' => Yii::t('language', 'Language'), 'items' => [
+            ['label' => Yii::t('language', 'List of languages'), 'url' => ['/translatemanager/language/list']],
+            ['label' => Yii::t('language', 'Create'), 'url' => ['/translatemanager/language/create']],
+        ]],
+        ['label' => Yii::t('language', 'Scan'), 'url' => ['/translatemanager/language/scan']],
+        ['label' => Yii::t('language', 'Optimize'), 'url' => ['/translatemanager/language/optimizer']],
+        ['label' => Yii::t('language', 'Im-/Export'), 'items' => [
+            ['label' => Yii::t('language', 'Import'), 'url' => ['/translatemanager/language/import']],
+            ['label' => Yii::t('language', 'Export'), 'url' => ['/translatemanager/language/export']],
+        ]],
+    ];
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'items' => $menuItems,
+    ]);
+    NavBar::end();
+    ?>
 
-            <div class="container">
-                <?=
-                Breadcrumbs::widget([
-                    'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-                ])
-                ?>
-                <?php
-                foreach (Yii::$app->session->getAllFlashes() as $key => $message) {
-                    echo '<div class="alert alert-' . $key . '">' . $message . '</div>';
-                } ?>
-                <?= Html::tag('h1', Html::encode($this->title)) ?>
-                <?= $content ?>
-            </div>
-        </div>
+    <div class="container">
+        <?=
+        Breadcrumbs::widget([
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            'options' => [
+                'class' => 'breadcrumb',
+            ],
+            'tag' => 'nav',
+            'itemTemplate' => "<li class=\"breadcrumb-item\">{link}</li>\n",
+            'activeItemTemplate' => "<li class=\"breadcrumb-item active\">{link}</li>\n",
 
-        <footer class="footer">
-            <div class="container">
-                <p class="pull-left">&copy; Lajax TranslateManager <?= date('Y') ?></p>
-                <p class="pull-right"><?= Yii::powered() ?></p>
-            </div>
-        </footer>
-        <?php $this->endBody() ?>
-    </body>
+        ])
+        ?>
+        <?php
+        foreach (Yii::$app->session->getAllFlashes() as $key => $message) {
+            echo '<div class="alert alert-' . $key . '">' . $message . '</div>';
+        } ?>
+        <?= Html::tag('h1', Html::encode($this->title)) ?>
+        <?= $content ?>
+    </div>
+</div>
+
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">&copy; Lajax TranslateManager <?= date('Y') ?></p>
+        <p class="pull-right"><?= Yii::powered() ?></p>
+    </div>
+</footer>
+<?php $this->endBody() ?>
+</body>
 </html>
 <?php $this->endPage() ?>
